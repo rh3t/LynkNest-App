e172a655f028ada62ee01b4fbabd411857076aec
Rhet <meddakee2000@gmail.com>
2024-02-07 16:23:33 -0500

    Pushed ``dev-0.1.6``

--- home/script.js
+++ home/script.js
@@ -1,6 +1,33 @@
-// Check If Logged In
-document.addEventListener('DOMContentLoaded', function(){
-    if(!localStorage.getItem('loggedIn?')){
-        this.location.href = '/';
+// Get Elements From DOM
+const urlInput = document.getElementById('urlInput');
+const linkList = document.getElementById('linkList');
+const addButton = document.getElementById('addButton');
+const importButton = document.getElementById('importButton');
+const exportButton = document.getElementById('exportButton');
+
+// Events
+addButton.addEventListener('click', function(){
+
+    // Check Value of urlInput
+    if(urlInput.value.includes('https://') && urlInput.value.length > 8){
+
+        // Create Element
+        var element = document.createElement('p');
+        element.innerHTML = urlInput.value;
+        element.classList.add('link');
+
+        // Open Link On Click
+        element.addEventListener('click', function(){
+            window.open(element.innerHTML);
+        })
+
+        // Append Element
+        linkList.appendChild(element);
+
+    } else {
+
+        window.alert('Invalid URL');
+
     }
+
 })
