dda32db64d955ac511c0dbae004e250318c7e236
Rhet <meddakee2000@gmail.com>
2024-02-07 05:45:26 -0500

    Pushed ``pre-0.1``

---
+++ assets/scripts/version-handler.js
@@ -0,0 +1,22 @@
+// Init
+document.addEventListener('DOMContentLoaded', function(){
+
+    // Fetch Version
+    fetch('assets/manifest.json').then(response => response.json()).then(data => {
+
+        // Log Version
+        console.log(`[DEBUG] App Version: ${data.build_type}-${data.build_version}`);
+
+        // Create Element
+        const versionElement = document.createElement('p');
+
+        // Edit Element
+        versionElement.classList.add('version-label');
+        versionElement.innerHTML = `${data.build_type}-${data.build_version}`;
+
+        // Append Element to DOM
+        document.body.appendChild(versionElement);
+
+    })
+
+})
